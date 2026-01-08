1babf17c1edcc2b21c84bb6d566e96f3724d9222
shaan11s <65747263+shaan11s@users.noreply.github.com>
2026-01-08 14:11:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -191,17 +191,17 @@ https://docs.google.com/spreadsheets/d/1kT1PrkvdGK6DrQ74_F0CktXWppVn2xSxcwtMDnH8
     // Array of artwork images
     // HEY THE HOME PAGE PIXEL ERASER THINGY IS HERE
     // CHANGE UR PHOTOS HERE
+        //  'imgs/Polilla.jpeg',
+            //    'imgs/SoldadoCaído.jpeg',
+       // 'imgs/MiradaEnRuinas.jpeg',
+       // 'imgs/Gallardía.jpeg',
     const artworks = [
         'imgs/ladamaA.JPG',
         'imgs/nadapersonal2.JPG',
-      //  'imgs/Polilla.jpeg',
         'imgs/Pugna.jpeg',
         'imgs/VueloInterno.jpeg',
-    //    'imgs/SoldadoCaído.jpeg',
-       // 'imgs/MiradaEnRuinas.jpeg',
-       // 'imgs/Gallardía.jpeg',
-        'imgs/ensueño.jpeg',
-        'imgs/El-Incrédulo.jpeg',
+        'imgs/ensueno.jpeg',
+        'imgs/El-Incredulo.jpeg',
         'imgs/Reino-Fungi.jpeg',
         'imgs/creature1.JPG'
     ];
